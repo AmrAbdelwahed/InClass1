ae79c52126125a6f3cc5535f3cadf1d1ca8fc7fa
AmrAbdelwahed <abdelwa0@sheridancollege.ca>
2022-02-01 13:57:04 -0500

    created an instance of Card

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  * Student ID: 991664837
+ * Comments from Amr Abdelwahed
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
