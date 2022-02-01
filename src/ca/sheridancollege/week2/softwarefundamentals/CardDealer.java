a42929bbaf133709b46970ef131e2ad05f82669e
AmrAbdelwahed <abdelwa0@sheridancollege.ca>
2022-02-01 13:35:15 -0500

    created an instance of Card

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Student ID: 991664837
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
@@ -15,5 +16,6 @@ public class CardDealer
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        Card card1 = new Card("Amr Abdelwahed",9);
     }
 }
